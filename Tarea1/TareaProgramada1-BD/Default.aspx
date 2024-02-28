2aaca517cd3f339861f9ffce06e66592f0018de1
Pamreth29 <124837434+Pamreth29@users.noreply.github.com>
2024-02-28 01:49:30 -0600

    BotonInsertar

--- Tarea1/TareaProgramada1-BD/Default.aspx
+++ Tarea1/TareaProgramada1-BD/Default.aspx
@@ -1,11 +1,30 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TareaProgramada1_BD._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
     <div class="jumbotron">
+        <asp:Panel runat="server" ID="pnlDatoEmpleado">
+            <asp:GridView ID="gvdEmpleados" runat="server" AutoGenerateColumns="true">
+        
+            </asp:GridView>
+            <asp:button ID ="btnNuevo" Text ="Insertar" runat ="server"  OnClick ="btnNuevo_Click" />
+        </asp:Panel>
 
-        <asp:GridView ID="gvdEmpleados" runat="server" AutoGenerateColumns="true">
-        </asp:GridView>
+        <asp:Panel ID="pnlAltaEmpleado" runat="server" Visible="false">
+           
+            <div>
+                <asp:Label ID="IblNombre" Text="Nombre" runat="server"></asp:Label>
+                 <asp:TextBox ID="txtNombre" runat="server" />
+            </div>
+
+            <div>
+                <asp:Label ID="lbSalario" Text="Salario" runat="server"></asp:Label>
+                <asp:TextBox ID="txtSalario" runat="server" />
+            </div>
+            <asp:Button ID="btnInsertar" runat="server" Text="Insertar" OnClick="btnGuardar_Click" />
+
+        </asp:Panel> 
+
+
 
     </div>
     
--- Tarea1/TareaProgramada1-BD/Default.aspx.designer.cs
+++ Tarea1/TareaProgramada1-BD/Default.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace TareaProgramada1_BD
     {
 
         /// <summary>
+        /// pnlDatoEmpleado control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlDatoEmpleado;
+
+        /// <summary>
         /// gvdEmpleados control.
         /// </summary>
         /// <remarks>
@@ -22,5 +31,68 @@ namespace TareaProgramada1_BD
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvdEmpleados;
+
+        /// <summary>
+        /// btnNuevo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNuevo;
+
+        /// <summary>
+        /// pnlAltaEmpleado control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlAltaEmpleado;
+
+        /// <summary>
+        /// IblNombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label IblNombre;
+
+        /// <summary>
+        /// txtNombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// lbSalario control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbSalario;
+
+        /// <summary>
+        /// txtSalario control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSalario;
+
+        /// <summary>
+        /// btnInsertar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnInsertar;
     }
 }
